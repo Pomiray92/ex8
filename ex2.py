073c11d38b4c2111d020f9da74398c3b9b8900de
writehereyourname <writehereyouremail>
2022-08-24 15:58:42 +0200

    new

---
+++ ex2.py
@@ -0,0 +1,46 @@
+
+
+    #Task1
+
+from unittest import skip
+
+
+text3 = "Berlin is a world city of culture, politics, media and science."
+print(len(text3))
+
+    #Task2
+
+text = "Berlin is a world city of culture, politics, media and science."
+print(text[0], text[-1])   
+
+    #Task3
+
+text2 = "Berlin is a world city of culture, politics, media and science."
+print(text2[:3].upper())
+
+    #Task4
+
+text4 = """Berlin straddles the banks of the Spree, 
+           which flows into the Havel (a tributary of the Elbe) 
+           in the western borough of Spandau."""
+print(text4[-3:])
+
+    #Task5
+
+text5 = ("""Create a variable called ```text``` to store the data: 
+        ```Berlin straddles the banks of the Spree, which flows into 
+        the Havel (a tributary of the Elbe) in the western borough of 
+        Spandau.""")
+print("last ten characters:" + text5[-10:])
+
+    #Task6
+
+text6 = ("---Python programming---")
+print(text6.strip("-"))
+
+    #Task7
+
+Firstname = "Mary"
+Lastname = "Mat"
+print
+
